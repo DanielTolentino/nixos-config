1defd2893d27c41ee98fa6ed83b443eeface6b53
Daniel Tolentino <danieltolentino@protonmail.com>
2023-08-07 17:49:37 -0300

    Add input on system

--- lib/mkvm.nix
+++ lib/mkvm.nix
@@ -1,11 +1,14 @@
 # This function creates a NixOS system based on our VM setup for a
 # particular architecture.
-name: { nixpkgs, home-manager, system, user, overlays }:
+name: { nixpkgs, home-manager, vscode-server,  system, user, overlays }:
 
 nixpkgs.lib.nixosSystem rec {
   inherit system;
 
   modules = [
+  	vscode-server.nixosModules.default
+    ({ config, pkgs, ... }: {
+    services.vscode-server.enable = true;
     # Apply our overlays. Overlays are keyed by system type so we have
     # to go through and apply our system type. We do this first so
     # the overlays are available globally.
