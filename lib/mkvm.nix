04c915bf0a6e6df2ac864bede042bd682be90ff1
Daniel Tolentino <danieltolentino@protonmail.com>
2023-08-07 17:51:06 -0300

    Add input on system

--- lib/mkvm.nix
+++ lib/mkvm.nix
@@ -8,7 +8,7 @@ nixpkgs.lib.nixosSystem rec {
   modules = [
   	vscode-server.nixosModules.default{
     services.vscode-server.enable = true;
-    };
+    }
     # Apply our overlays. Overlays are keyed by system type so we have
     # to go through and apply our system type. We do this first so
     # the overlays are available globally.
